6ff4892a6b36ac3845a8ddd1a9bb4001337bd84d
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2016-10-16 09:58:31 +0900

    Modified.

--- App/Test/main.cpp
+++ App/Test/main.cpp
@@ -12,9 +12,15 @@ int main( int argc, char** argv )
     ReadBDML::BDMLData bdml( argv[1] );
     bdml.print( std::cout );
 
+//    const size_t cindex = 300;
 //    screen.registerObject( ReadBDML::ImportLineObjectByComponent( bdml, 300 ) );
+
+//    const size_t cindex = 300;
+//    const size_t sindex = 0;
 //    screen.registerObject( ReadBDML::ImportLineObjectBySequence( bdml, 300, 0 ) );
-    screen.registerObject( ReadBDML::ImportLineObjectByTime( bdml, 49 ) );
+
+    const float time = 49;
+    screen.registerObject( ReadBDML::ImportLineObjectByTime( bdml, time ) );
 
     return app.run();
 }
